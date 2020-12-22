258f34ac4681f1a940202df36a9d1eb2b34c0195
Ruslan Valeev <ruvaleev@gmail.com>
2020-12-22 17:41:35 +0700

    Deploy config added

--- pm2.config.js
+++ pm2.config.js
@@ -5,5 +5,15 @@ module.exports = {
     env: {
       NODE_ENV: 'production'
     }
-  }]
+  }],
+  deploy: {
+    production: {
+      user: 'ec2-user',
+      host: ['18.141.159.219'],
+      ref: 'origin/lesson_14',
+      repo: 'git@github.com:ruvaleev/publisher_redux.git',
+      path: '/home/ec2-user/publisher-app',
+      'post-deploy': 'npm run build:client; npm run build:server; pwd; pm2 startOrRestart pm2.config.js'
+    }
+  }
 }
